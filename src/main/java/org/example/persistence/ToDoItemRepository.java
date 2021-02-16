7d46208326a4ec0e9516d34945ad6ebb44153317
Bodea Tudor <bodea.tudor99@yahoo.com>
2021-02-16 16:54:34 +0200

    *added persistence layer with a ToDoItemRepository and a method for creating an item in the database

---
+++ src/main/java/org/example/persistence/ToDoItemRepository.java
@@ -0,0 +1,26 @@
+package org.example.persistence;
+
+import org.example.config.DatabaseConfig;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
+public class ToDoItemRepository {
+
+    public void createToDoItem(String description, LocalDateTime deadline) throws SQLException, IOException, ClassNotFoundException {
+
+        String insertSql = "INSERT INTO to_do_item (description, deadline) VALUES (?, ?)";
+
+        try (Connection connection = DatabaseConfig.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(insertSql)
+        ) {
+            preparedStatement.setString(1, description);
+            preparedStatement.setDate(2, java.sql.Date.valueOf(deadline.toLocalDate()));
+
+            preparedStatement.executeUpdate();
+        }
+    }
+}
