7f52d701a1a99bc454af45e7582d878339ab40a8
Bodea Tudor <bodea.tudor99@yahoo.com>
2021-02-16 17:32:47 +0200

    *added a GET endpoint for retrieving items

--- src/main/java/org/example/web/ToDoItemServlet.java
+++ src/main/java/org/example/web/ToDoItemServlet.java
@@ -1,6 +1,7 @@
 package org.example.web;
 
 import org.example.config.ObjectMapperConfig;
+import org.example.domain.ToDoItem;
 import org.example.service.ToDoItemService;
 import org.example.transfer.CreateItemRequest;
 
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 @WebServlet("/to-do-items")
 public class ToDoItemServlet extends HttpServlet {
@@ -28,5 +30,22 @@ public class ToDoItemServlet extends HttpServlet {
             resp.sendError(500, "Internal server error: " + e.getMessage());
         }
 
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        try {
+            List<ToDoItem> toDoItems = toDoItemService.getToDoItems();
+
+            String responseJson = ObjectMapperConfig.getObjectMapper().writeValueAsString(toDoItems);
+
+            resp.getWriter().print(responseJson);
+            resp.getWriter().flush();
+            resp.getWriter().close();
+
+        } catch (SQLException | ClassNotFoundException e) {
+            resp.sendError(500, "Internal server error: " + e.getMessage());
+        }
     }
 }
