9d4c73e42807995283a03dd5b8ea85c6d2db8ecf
Bodea Tudor <bodea.tudor99@yahoo.com>
2021-02-16 17:24:32 +0200

    *added configuration for an ObjectMapper for global use in the application

---
+++ src/main/java/org/example/config/ObjectMapperConfig.java
@@ -0,0 +1,17 @@
+package org.example.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+public class ObjectMapperConfig {
+
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+    static {
+        objectMapper.registerModule(new JavaTimeModule());
+    }
+
+    public static ObjectMapper getObjectMapper() {
+        return objectMapper;
+    }
+}
